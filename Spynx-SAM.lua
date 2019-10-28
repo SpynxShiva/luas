88ce710c83d6ad9fcdd99fb716d82a567dd0aeba
SpynxShiva <45853647+SpynxShiva@users.noreply.github.com>
2019-10-27 21:24:04 -0400

    Update Spynx-SAM.lua

--- Spynx-SAM.lua
+++ Spynx-SAM.lua
@@ -9,6 +9,7 @@ function get_sets()
     -- Load and initialize the include file.
     include('Mote-Include.lua')
 	include('organizer-lib')
+	include('Mote-TreasureHunter')
 end
 
 -- Setup vars that are user-independent.  state.Buff vars initialized here will automatically be tracked.
@@ -22,23 +23,15 @@ end
 
 -- Setup vars that are user-dependent.  Can override this function in a sidecar file.
 function user_setup()
-    state.OffenseMode:options('Normal', 'MidAcc', 'HighAcc')
+    state.OffenseMode:options('Normal', 'LowAcc', 'MidAcc', 'HighAcc')
     state.HybridMode:options('Normal', 'DT')
-    state.RangedMode:options('Normal', 'Acc')
-    state.WeaponskillMode:options('Normal', 'Acc', 'Mod')
-    state.PhysicalDefenseMode:options('DT')
+    state.WeaponskillMode:options('Normal', 'Acc')
+    state.PhysicalDefenseMode:options('PDT')
+	state.MagicalDefenseMode:options('MDT')
 
 
-    gear.default.weaponskill_neck = "Asperity Necklace"
-    gear.default.weaponskill_waist = "Caudata Belt"
-	
-	gear.Valor_TP_body = { name="Valorous Mail", augments={'Accuracy+26','"Store TP"+6','DEX+10','Attack+15',}}
-	gear.Ryuo_TP_legs = { name="Ryuo Hakama", augments={'Accuracy+20','"Store TP"+4','Phys. dmg. taken -3',}}
+	send_command('bind ^t gs c cycle TreasureMode')
 
-	-- Additional local binds
-    send_command('bind ^z input /ja "Spirit Jump" <t>')
-	send_command('bind ^x input /ja "Soul Jump" <t>')
-    
     select_default_macro_book()
 	set_lockstyle()
 end
@@ -51,6 +44,11 @@ end
 
 -- Define sets and vars used by this job file.
 function init_gear_sets()
+
+	sets.TreasureHunter = {
+		body="Volte Jupon",		-- 2
+		waist="Chaac Belt", 	-- 1
+	}
     
 	--------------------------------------
     -- Precast sets
@@ -64,18 +62,15 @@ function init_gear_sets()
 		back=gear.SAM_TP_Cape,
 	}
 
-
     -- Weaponskill sets
-
-    -- Default set for any weaponskill that isn't any more specifically defined
-    sets.precast.WS = {
+  sets.precast.WS = {
 		ammo="Knobkierrie",
 		head=gear.Valor_WSD_head,
 		body="Sakonji Domaru +3",
 		hands=gear.Valor_WSD_hands,
 		legs="Wakido Haidate +3",
 		feet=gear.Valor_WSD_feet,
-		neck="Fotia Gorget",
+		neck="Sam. Nodowa +2",
 		waist="Fotia Belt",
 		ear1="Moonshade Earring",
 		ear2="Ishvara Earring",
@@ -86,13 +81,13 @@ function init_gear_sets()
 
 	sets.precast.WS.Acc = set_combine(sets.precast.WS,{ring2="Regal Ring"})
 	
-    -- Specific weaponskill sets.  Uses the base set if an appropriate WSMod version isn't found.
+    -- Specific weaponskill sets.
     sets.precast.WS['Tachi: Jinpu'] = {
 		ammo="Knobkierrie",
 		head=gear.Valor_WSD_head,
 		neck="Fotia Gorget",
-		ear1="Ishvara Earring",
-		ear2="Moonshade Earring",
+		ear1="Moonshade Earring",
+		ear2="Ishvara Earring",
 		body="Found. Breastplate",
 		hands="Founder's Gauntlets",
 		ring1="Karieyh Ring +1",
@@ -103,40 +98,52 @@ function init_gear_sets()
 		back=gear.SAM_WS_Cape,
 	}
 	
+	sets.precast.WS['Tachi: Shoha'] = {
+		ammo="Knobkierrie",
+		head="Flamma Zucchetto +2",
+		neck="Sam. Nodowa +2",
+		ear1="Moonshade Earring",
+		ear2="Ishvara Earring",
+		body="Sakonji Domaru +3",
+		hands=gear.Valor_WSD_hands,
+		ring1="Flamma Ring",
+		ring2="Niqmaddu Ring",
+		waist="Fotia Belt",
+		legs="Wakido Haidate +3",
+		feet="Flam. Gambieras +2",
+		back=gear.SAM_WS_Cape,
+	}
+	
+	sets.precast.WS['Stardiver'] = sets.precast.WS['Tachi: Shoha']
+	
+	sets.WSfullTP = {ear1='Odnowa Earring +1'}
+	sets.WSfullTPnight = {ear1='Lugra Earring +1'}
+	
+	
     --------------------------------------
     -- Idle/resting/defense sets
     --------------------------------------
-    -- Idle sets (default idle set not needed since the other three are defined, but leaving for testing purposes)
+    -- Idle sets
     sets.idle = {						--	PDT		MDT
-		ammo="Staunch Tathlum +1",		--	2		2
-		head="Loess Barbuta +1",		--	10		10
+		ammo="Staunch Tathlum +1",		--	3		3
+		head="Kendatsuba Jinpachi +1",	--			
 		neck="Loricate Torque +1",		--	6		6
 		left_ear="Etiolation Earring",	-- 			3
-		right_ear="Odnowa Earring +1",	--			2
-		body="Chozoron Coselete",		--	8		8
-		hands="Macabre Gauntlets +1",	--	4
+		right_ear="Genmei Earring",		--	2
+		body="Wakido Domaru +3",		--	8		8
+		hands="Wakido Kote +3",			--	
 		ring1="Defending Ring",			--	10		10	
-		ring2=gear.dark_ring,			--	6		4
-		legs=gear.Ryuo_TP_legs,			--	3
-		feet="Loyalist Sabatons",		--	5
+		ring2="Gelatinous Ring +1",		--	7		-1
+		legs="Kendatsuba Hakama +1",	--	
+		feet="Kendatsuba Sune-ate +1",	--	
 		waist="Flume Belt +1",			--	4
-		back="Moonbeam Cape",			--	5		5
-	}									--	63		50	
+		back=gear.SAM_TP_Cape,			--	10		
+	}									--	50		29	
 	
     -- Defense sets
-    sets.defense.DT = sets.idle
+    sets.defense.PDT = sets.idle
 	sets.defense.MDT = sets.idle
     
-    -- Melee sets
-    sets.DT = {
-		ammo="Staunch Tathlum +1",	--	2		2
-		neck="Loricate Torque +1",	--	6		6
-		ring1="Regal Ring",
-		ring2="Defending Ring",		--	10		10	
-		back=gear.SAM_TP_Cape,		--	10		
-	}								--	34		22
-	
-	
 	sets.engaged = {
 		ammo="Ginsen",
 		head="Flam. Zucchetto +2",
@@ -153,34 +160,42 @@ function init_gear_sets()
 		back=gear.SAM_TP_Cape
 	}
 	
-	sets.engaged.MidAcc = set_combine(sets.engaged,{
-		body="Kendatsuba Samue +1",
-		legs="Kendatsuba Hakama +1",
-		feet="Flam. Gambieras +2",
+	sets.engaged.LowAcc = set_combine(sets.engaged,{
+		neck="Sam. Nodowa +2",
+		body="Ken. Samue +1",
+		legs="Ken. Hakama +1"
+	})
+	
+	sets.engaged.MidAcc = set_combine(sets.engaged.LowAcc,{
+		ear1="Cessance Earring",
+		feet="Flam. Gambieras +2"
 	})
 	
 	sets.engaged.HighAcc = set_combine(sets.engaged.MidAcc,{
-		ear1="Cessance Earring",
-		ring2="Regal Ring"
+		head="Ken. Jinpachi +1",
+		ring1="Regal Ring",
+		feet="Ken. Sune-Ate +1"
 	})
 	
-	
-	sets.idle.Town = set_combine(sets.engaged.MidAcc,{})
+	sets.idle.Town = sets.engaged.MidAcc
 		
-	sets.engaged.DT = set_combine(sets.engaged,sets.DT)
-    sets.engaged.MidAcc.DT = set_combine(sets.engaged.MidAcc,sets.DT)
-	sets.engaged.HighAcc.DT = set_combine(sets.engaged.HighAcc,sets.DT)
+	sets.engaged.DT = sets.idle
+	sets.engaged.LowAcc.DT = sets.idle
+	sets.engaged.MidAcc.DT = sets.idle
+	sets.engaged.HighAcc.DT = sets.idle
 	
 	
 	sets.WakeSleep = {neck="Vim Torque +1"}
 	
 	-- Orgnizer set
 	organizer_items = {
+		-- Weapons
 		gkt1="Masamune",
 		gkt2="Dojikiri Yasutsuna",
 		gkt3="Norifusa +1",
 		polearm="Shining One",
 		grip="Utu Grip",
+		-- Food and meds
 		acc_food="Sublime Sushi",
 		atk_food="Red Curry Bun",
 		remedy="Remedy"
@@ -193,24 +208,20 @@ end
 -- Job-specific hooks for standard casting events.
 -------------------------------------------------------------------------------------------------------------------
 
--- Run after the general precast() is done.
-function job_post_precast(spell, action, spellMap, eventArgs)
-    
-end
-
 -- Run after the general midcast() set is constructed.
 function job_post_midcast(spell, action, spellMap, eventArgs)
-    
-end
-
--- Set eventArgs.handled to true if we don't want any automatic gear equipping to be done.
-function job_aftercast(spell, action, spellMap, eventArgs)
-    
-end
-
--- Called after the default aftercast handling is complete.
-function job_post_aftercast(spell, action, spellMap, eventArgs)
-
+    if spell.type == 'WeaponSkill' then
+		-- Already full tp -> swap moonshade out
+		if player.tp > 2900 or ( player.tp > 2400 and player.equipment.main == 'Dojikiri Yasutsuna') then
+			if world.time >= (17*60) or world.time <= (7*60) then
+                add_to_chat(122, 'WS on Full TP + Dusk to Dawn')
+				equip(sets.WSfullTPnight)
+            else
+				add_to_chat(122, 'WS on Full TP')
+				equip(sets.WSfullTP)
+            end
+		end     
+	end
 end
 
 -------------------------------------------------------------------------------------------------------------------
@@ -227,40 +238,21 @@ function job_buff_change(buff, gain)
         end
     end
 	
-	if gain and status == "sleep" then  
-		add_to_chat(4,'Swapping in Vim Torque to take you up!')
+	if gain and buff:lower() == "sleep" then  
+		add_to_chat(4,'Swapping in Vim Torque to wake you up!')
 		equip(sets.WakeSleep)
-	elseif not gain and status == "sleep" then
+		disable('neck')
+	elseif not gain and buff:lower() == "sleep" then
 		add_to_chat(4,'Removing Vim Torque')
 		handle_equipping_gear(player.status)
+		enable('neck')
 	end
 
-end
-
-function customize_idle_set(idleSet)
-    return idleSet
-end
-
-
-function customize_melee_set(meleeSet)
-    return meleeSet
-end
-
-
--- Called by the 'update' self-command.
-function job_update(cmdParams, eventArgs)
-    
 end
 
 -------------------------------------------------------------------------------------------------------------------
 -- Utility functions specific to this job.
 -------------------------------------------------------------------------------------------------------------------
-
--- State buff checks that will equip buff gear and mark the event as handled.
-function check_buff(buff_name, eventArgs)
-    
-end
-
 
 -- Select default macro book on initial load or subjob change.
 function select_default_macro_book()
